7e81833073235eed8e85239007bfe9b18b1b7a6c
ArGilfea <philippe.laporte.3@umontreal.ca>
2022-10-03 19:29:12 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -12,10 +12,36 @@ import matplotlib.pyplot as plt
 ###
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
 from functools import partial
 ###
+DISPLAY_HEIGHT = 35
+BUTTON_SIZE = 40
 
 class Window(QMainWindow):
+    def _createButtons(self):
+        self.buttonMap = {}
+        buttonsLayout = QGridLayout()
+        keyBoard = [
+            ["7", "8", "9", "/", "C"],
+            ["4", "5", "6", "*", "("],
+            ["1", "2", "3", "-", ")"],
+            ["0", "00", ".", "+", "="],
+        ]
+
+        for row, keys in enumerate(keyBoard):
+            for col, key in enumerate(keys):
+                self.buttonMap[key] = QPushButton(key)
+                self.buttonMap[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+                buttonsLayout.addWidget(self.buttonMap[key], row, col)
+
+        self.generalLayout.addLayout(buttonsLayout)
+    def _createDisplay(self):
+        self.display = QLineEdit()
+        self.display.setFixedHeight(DISPLAY_HEIGHT)
+        self.display.setAlignment(Qt.AlignRight)
+        self.display.setReadOnly(True)
+        self.generalLayout.addWidget(self.display)        
     def _createStatusBar(self):
         status = QStatusBar()
         status.showMessage("Hey there!")
@@ -31,26 +57,16 @@ class Window(QMainWindow):
                 msgLabel.setText(f"Hello, {name}!")
 
         super().__init__(parent=None)
-        #self.setMinimumSize(500, 400)
+        self.setMinimumSize(500, 400)
         self.setWindowTitle("My GUI")
-        dialogLayout = QVBoxLayout()
-        formLayout = QFormLayout()
-        self.name = formLayout.addRow("Name:", QLineEdit())
-        dialogLayout.addLayout(formLayout)
-        button = QPushButton("Click Me!")
-        button2 = QPushButton("Greetings")
-        button.clicked.connect(on_button_clicked)
-        button2.clicked.connect(partial(greet,self.name))
-        dialogLayout.addWidget(button)
-        msgLabel = QLabel("")
-        dialogLayout.addWidget(button2)
-        dialogLayout.addWidget(msgLabel)
-        btn = QPushButton("Close")
-        btn.clicked.connect(self.close)
-        dialogLayout.addWidget(btn)
-        self._createStatusBar()
-        self.setLayout(dialogLayout)
+        self.generalLayout = QVBoxLayout()
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
+        self.setCentralWidget(centralWidget)
+        self._createDisplay()
+        self._createButtons()
 
+        msgLabel = QLabel("")
 
 
 os.system('clear')
